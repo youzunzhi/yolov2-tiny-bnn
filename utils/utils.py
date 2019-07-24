3342772731d125eeaaaed2551e756c99badecbd5
youzunzhi <youzunzhi@163.com>
2019-07-24 13:31:20 +0800

    just pretrained default and change seen

--- utils/utils.py
+++ utils/utils.py
@@ -16,6 +16,7 @@ class Options(object):
         # data and model
         parser.add_argument("--data_cfg", type=str, default="cfg/voc.data", help="path to data cfg file")
         parser.add_argument("--model_cfg", type=str, default="cfg/yolov2-tiny-voc.cfg", help="path to model cfg file")
+        parser.add_argument("--weights_file", type=str, default="weights/yolov2-tiny-voc.weights", help="path to weights file")
         # hyper parameters
         parser.add_argument("--batch_size", type=int, default=64, help="size of each image batch")
         parser.add_argument("--nms_thresh", type=float, default=0.4, help="the threshold of non-max suppresion algorithm")
@@ -38,8 +39,6 @@ class Options(object):
             parser.add_argument("--pretrained_weights", type=str, default="weights/darknet.weights", help="path to pretrained weights file")
 
         else:
-            parser.add_argument("--weights_file", type=str, default="weights/yolov2-tiny-voc.weights",
-                                help="path to weights file")
             parser.add_argument("--conf_thresh", type=float, default=0, help="only keep detections with conf higher than conf_thresh")
 
         self.options = parser.parse_args()
